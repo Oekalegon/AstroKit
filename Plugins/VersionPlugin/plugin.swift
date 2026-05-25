23fb6e2d0f3f57caa87a04b121f4ecda290ac7aa
Oekalegon <don.willems@icloud.com>
2026-05-25 16:41:27 +0200

    Changed to per commit

--- Plugins/VersionPlugin/plugin.swift
+++ Plugins/VersionPlugin/plugin.swift
@@ -3,17 +3,17 @@ import PackagePlugin
 @main
 struct VersionPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-        let versionFile = context.package.directory.appending("version.txt")
-        let gitHead     = context.package.directory.appending(".git/HEAD")
-        let outputFile  = context.pluginWorkDirectory.appending("Version.generated.swift")
-        let tool        = try context.tool(named: "version-tool")
+        let versionFile   = context.package.directory.appending("version.txt")
+        let gitCommitMsg  = context.package.directory.appending(".git/COMMIT_EDITMSG")
+        let outputFile    = context.pluginWorkDirectory.appending("Version.generated.swift")
+        let tool          = try context.tool(named: "version-tool")
 
         return [
             .buildCommand(
                 displayName: "Generating Version.swift (\(target.name))",
                 executable: tool.path,
                 arguments: [versionFile.string, outputFile.string],
-                inputFiles: [versionFile, gitHead],
+                inputFiles: [versionFile, gitCommitMsg],
                 outputFiles: [outputFile]
             )
         ]
